69733765975d2fdbf0656bb2402e5048c84178cd
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2022-10-14 12:38:07 +0300

    Add project files.

---
+++ Stack/Stack.cpp
@@ -0,0 +1,23 @@
+// Stack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct stackNode {
+    int data;
+    stackNode* next;
+public:
+    stackNode(int data) :data(data), next (nullptr){}
+};
+
+
+int main()
+{
+    
+
+
+}
+
+
